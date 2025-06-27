9d70de615e43263a4bc650d5e25abd5a1ef1dcdd
Happy1speed <144567994+Happy1speed@users.noreply.github.com>
2025-06-27 14:33:01 -0700

    tech should be artillery not foundry

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,7 @@
 
 data.raw.recipe["artillery-shell"].results = {{type="item", name="artillery-shell", amount=5}}
 
-table.insert(data.raw.technology["foundry"].effects, {
+table.insert(data.raw.technology["artillery"].effects, {
     recipe = "artillery-shell-from-steel",
     type = "unlock-recipe"
 })
